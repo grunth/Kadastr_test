956e32da7915d8dc4e11b0aae6b156b16d4b0933
User <User@Noyt-Server>
2018-08-18 13:40:11 +0300

    -добавлены библиотеки java.util,java.awt.Desktop -создана функция "просмотр xml файла", используется при нажатии -определена корневая директория выполнения программы для сохранения xml файла (должно быть не зависимо от расположения) -создан массив ключ-значения для вставления значений в свойства веток xml -добавлено несколько веток при построении xml (CadastralBlock,CadastralNumber)

--- src/ga/osipov/xml_create/XmlFileCreator.java
+++ src/ga/osipov/xml_create/XmlFileCreator.java
@@ -1,6 +1,9 @@
 package ga.osipov.xml_create;
-
-import java.io.File;
+//массив ключ+значение
+import java.util.*;
+//открытие файла
+import java.awt.Desktop;
+import java.io.*;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -9,13 +12,34 @@ import javax.xml.bind.Marshaller;
 import XML.MP_v06.MP;
 import XML.MP_v06.MP.Package;
 import XML.MP_v06.MP.Package.FormParcels;
+import XML.MP_v06.TCadastralNumbersInp;
 import XML.MP_v06.TNewParcel;
 
 public class XmlFileCreator {
-
+	//открытие файла
+	public static void open_xml(String fileName) {
+		Desktop desktop = null;
+		if (Desktop.isDesktopSupported()) {
+		    desktop = Desktop.getDesktop();
+		}
+		try {
+		    desktop.open(new File(fileName));
+		} catch (IOException ioe) {
+		    ioe.printStackTrace();
+		}
+	}
 	
-	public static void create (String NewParcel) {
-		String fileName = "P:/data/eclipse-workspace/MP_01.xml";
+	public static void create (String NewParcel) 
+	//обработка исключений для открытия файла xml
+	//throws IOException 
+	{
+		//массив ключ-значение
+		Map<String,String> vs=new HashMap<String,String>();
+		vs.put("CadastralBlock", "test");
+		vs.put("CadastralNumber", "test");
+		
+		//автомотическое определение расположение файла в проекте
+		String fileName = new File(".").getAbsolutePath()+"/MP_01.xml";
 
 		MP Mplan = new MP();
 		Mplan.setGUID("29fd5fa8-4334-4dbe-b027-294cc2505bbd");
@@ -25,18 +49,25 @@ public class XmlFileCreator {
 		
 		Package p = new Package();
 		Mplan.setPackage(p);
-		
+		//Образование участков
 		FormParcels fp = new FormParcels();
 		fp.setMethod("3");
 		p.setFormParcels(fp);
 		
-		TNewParcel TNP = new TNewParcel();
-		TNP.setDefinition(NewParcel);
-		fp.getNewParcel().add(TNP);
+		TNewParcel NP = new TNewParcel();
+		NP.setDefinition(NewParcel);
+		fp.getNewParcel().add(NP);
+		NP.setCadastralBlock(vs.get("CadastralBlock"));
 		
+		TCadastralNumbersInp cni=new TCadastralNumbersInp();
+		cni.getCadastralNumber().add(vs.get("CadastralNumber"));
+		NP.setPrevCadastralNumbers(cni);
 		
 		
 		convertObjectToXml(Mplan, fileName);
+		open_xml(fileName);
+		//ProcessBuilder pb = new ProcessBuilder("Notepad.exe", fileName);
+		//pb.start();
 	}
 	
 		// сохраняем объект в XML файл
@@ -50,7 +81,9 @@ public class XmlFileCreator {
 				marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
 
 				// маршаллинг объекта в файл
-				marshaller.marshal(Mplan, new File(filePath));
+				File f=new File(filePath);
+				marshaller.marshal(Mplan,f);
+				
 			} catch (JAXBException e) {
 				e.printStackTrace();
 			}
